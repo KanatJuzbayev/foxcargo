fef3bee360d719e716e307ccbb01ae4852c34fc0
Kanat Juzbayev <kanat.juzbayev@gmail.com>
2021-12-15 15:17:14 +0600

    feat: add request form view

---
+++ src/request.js
@@ -0,0 +1,30 @@
+//show Request Form
+const requestButtons = document.querySelectorAll('.request__button');
+const body = document.querySelectorAll('body');
+
+function showRequestForm() {
+  const request = document.querySelector('.request');
+  request.classList.add('request-active');
+  body.classList.add('lock');
+}
+
+requestButtons.forEach( requestButton => requestButton.addEventListener('click', showRequestForm));
+
+
+//close Request Form
+const closeRequestButtons = document.querySelectorAll('.request__close');
+
+function closeRequestForm() {
+  const request = document.querySelector('.request');
+  request.classList.remove('request-active');
+}
+closeRequestButtons.forEach( closeRequestButton => closeRequestButton.addEventListener('click', closeRequestForm));
+
+
+const request = document.querySelector('.request');
+
+request.addEventListener('click', function(e) {
+  if (!e.target.closest('.request-form')) {
+    closeRequestForm();
+  }
+});
